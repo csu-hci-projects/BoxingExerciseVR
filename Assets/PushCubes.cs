08f7e8f45892ae7a3cd7e5495eb05f63d0a771c1
maghalex <maghalex@rams.colostate.edu>
2020-03-04 16:52:49 -0700

    add c# script for pushing cubes + 2 more cubes

---
+++ Assets/PushCubes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushCubes : MonoBehaviour
+{
+    public float thrust = 1.0f;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.Find("Cube")){
+        rb.AddForce((transform.forward * -1) * thrust);
+        }
+        if(GameObject.Find("Cube1")){
+       //you can't yeild inside an update function
+       // yield WaitForSeconds (1);
+        rb.AddForce((transform.forward * -1) * thrust);
+        }
+        if(GameObject.Find("Cube2")){
+        //yield WaitForSeconds (0.25);
+        rb.AddForce((transform.forward * -1) * thrust);
+        }
+    }
+}
